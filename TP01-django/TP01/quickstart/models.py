b98dfa714e103735b1bc806d90f06f91bec142f6
fredfiesta <stephane.bngrd@gmail.com>
2022-03-09 16:38:13 +0100

    La création de la table Event

--- TP01-django/TP01/quickstart/models.py
+++ TP01-django/TP01/quickstart/models.py
@@ -1,3 +1,17 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
+
+class Event(models.Model):
+    date_de_creation = models.DateTimeField(auto_now_add=True)
+    nom_de_event = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['date_de_creation']
