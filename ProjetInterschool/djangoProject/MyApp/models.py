e8168bf52b09b56bc2a984dba994c1de6e21ac78
fredfiesta <stephane.bngrd@gmail.com>
2022-03-22 19:46:27 +0100

    pep8

--- ProjetInterschool/djangoProject/MyApp/models.py
+++ ProjetInterschool/djangoProject/MyApp/models.py
@@ -3,12 +3,14 @@ from datetime import date
 import django.contrib.auth.models
 from django.db import models
 
+
 class Event(models.Model):
     id = models.IntegerField
     nom = models.CharField(max_length=50)
     date = models.DateField()
     description = models.CharField(max_length=200)
     heure = models.DateTimeField()
+
 
 class Lieu(models.Model):
     nom = models.CharField(max_length=50)
@@ -18,6 +20,7 @@ class Commentaire(models.Model):
     contenu = models.CharField(max_length=50)
     date = models.DateField()
     heure = models.DateTimeField()
+
 
 class CommentaireLieu(models.Model):
     contenu = models.CharField(max_length=50)
--- ProjetInterschool/djangoProject/MyApp/serializers.py
+++ ProjetInterschool/djangoProject/MyApp/serializers.py
@@ -2,26 +2,30 @@ from . import models
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+
 class EventSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model=models.Event
-        fields='__all__' # ce qui yaura sur la vue
+        model = models.Event
+        fields = '__all__'  # ce qui yaura sur la vue
 
 
 class LieuSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model=models.Lieu
-        fields='__all__'
+        model = models.Lieu
+        fields = '__all__'
+
 
 class CommentaireSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model=models.Commentaire
-        fields='__all__'
+        model = models.Commentaire
+        fields = '__all__'
+
 
 class CommentaireLieuSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model=models.Commentaire
-        fields='__all__'
+        model = models.Commentaire
+        fields = '__all__'
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
